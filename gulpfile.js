8933316c9b13e89286c9364df3cdb102818caf8d
NidaZORLU <nidazorlunz@hotmail.com>
2021-09-09 10:11:54 +0300

    changegulpfilecodes

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,47 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const minifyCSS = require('gulp-csso');
-const minifyImg = require('gulp-imagemin');
-const minifyJS = require('gulp-uglify');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const del = require('del');
+const sass = require('gulp-sass');
 
-gulp.task('browser-sync', () => {
-    browserSync.init({
-        server: {
-            baseDir: "dist"
-        }
-    });
+gulp.task('message',()=>{
+    console.log("Gulp running..");
 });
 
-gulp.task('css', () => {
-    return gulp.src('src/assets/scss/**/*.scss')
-        .pipe(sass({
-            outputStyle: 'nested',
-            precision: 10,
-            includePaths: ['.']
-        }).on('error', sass.logError))
-        .pipe(minifyCSS())
-        .pipe(autoprefixer())
-        .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
+
+gulp.task('kopyaHtml',()=>{
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('./dist/'));
+})
+
+gulp.task('jsMin',()=>{
+    gulp.src('src/assets/js/script.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('js', () => {
-    return gulp.src('src/assets/js/**/*.js')
-        .pipe(concat('app.min.js'))
-        .pipe(minifyJS())
-        .pipe(gulp.dest('dist/js'))
-        .pipe(browserSync.stream());
+gulp.task('cssMin',()=>{
+    gulp.src('src/assets/css/style.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('html', () => {
-    gulp.src('src/assets/**/*.html')
-        .pipe(gulp.dest('dist'))
-        .pipe(browserSync.stream());
+gulp.task('jsCon',()=>{
+    gulp.src('src/assets/js/script.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('img', () => {
-    gulp.src('src/img/**/*')
-        .pipe(minifyImg())
-        .pipe(gulp.dest('dist/img'));
+gulp.task('sass',()=>{
+    gulp.src('src/assets/sass/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('delete', () => del(['dist/css', 'dist/js', 'dist/img', 'dist/**/*.html']));
+gulp.task('izle',()=>{
+    gulp.watch('src/assets/css/**/*.css']);
+    gulp.watch('src/assets/js/**/*.js']);
+})
 
-gulp.task('watch', () => {
-    gulp.watch("src/assets/scss/**/*.scss", gulp.task('css'));
-    gulp.watch("src/assets/js/**/*.js", gulp.task('js'));
-    gulp.watch("src/assets/img/**/*.img", gulp.task('img'));
-    gulp.watch("src/assets/**/*.html", gulp.task('html'));
-});
-
-gulp.task('default', gulp.series('delete', gulp.parallel('html', 'css', 'js', 'img', 'browser-sync', 'watch')));
+gulp.task('default',['message','imageMin','kopyaHtml','cssMin' ,'jsCon' , 'sass']);
